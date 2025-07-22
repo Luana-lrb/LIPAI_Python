5aec4279de6de30fdd51539ced4dba614b93e32e
luana rodrigues <luana.borges1@ufu.br>
2025-07-22 14:04:14 -0300

    comentários

---
+++ semana_2/src/01-intro/aula3.py
@@ -0,0 +1,18 @@
+""" Aula 3 - Comentários"""
+
+# Comentário de única linha
+
+'''
+Isso é um
+comentário de
+múltiplas
+linhas
+
+'''
+
+# 1
+# 2
+# 3
+# 4
+
+#comentar tudo -> seleciona + Ctrl + ;
