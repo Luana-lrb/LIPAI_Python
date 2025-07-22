972a61b6e40ca2d5815c91fdf0846b0e2e5a00de
luana rodrigues <luana.borges1@ufu.br>
2025-07-22 14:03:30 -0300

    keywords e identificadores

---
+++ semana_2/src/01-intro/aula2.py
@@ -0,0 +1,34 @@
+""" Aula 2 - Keywords e identificadores"""
+
+# Palavras reservadas
+# True, False, None, and, import, lambda
+
+# Identificadores
+# nomes de variáveis, funçõies, classes
+# case sensitive
+# devem iniciar com uma letra ou _
+# não pode ter espaços em branco
+# não pode usar carcteres especiais !, @, #, $
+
+nome = 'Maria'
+idade = 30
+Nome = 'João'
+nome_Completo = 'Maria da silva'
+
+# Boas práticas (clean code)
+
+c = 10
+contador = 10
+
+soma = 10 + 20
+
+#Constante
+
+PI = 3.14
+
+MAIORIDADE = 18
+
+if idade >= MAIORIDADE:
+    print('Maior de idade')
+else:
+    print('Menor de idade')
