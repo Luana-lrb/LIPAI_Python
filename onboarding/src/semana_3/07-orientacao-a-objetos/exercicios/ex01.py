122df763278be239f9ca40978f6c3887d5d96eec
luana rodrigues <luana.borges1@ufu.br>
2025-08-06 16:56:34 -0300

    Inicio de orientação a objetos

---
+++ onboarding/src/semana_3/07-orientacao-a-objetos/exercicios/ex01.py
@@ -0,0 +1,13 @@
+""" Exercício 1 """
+class Aluno:
+    def __init__(self, dados):
+        aluno = dados.split(',')
+        if len(aluno) != 3:
+            raise ValueError("String deve ter 3 informações")
+        
+        self.prontuario = aluno[0].strip()
+        self.nome = aluno[0].strip()
+        self.email = aluno[0].strip()
+        pass
+    
+    
